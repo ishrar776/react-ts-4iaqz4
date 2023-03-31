870b051915e492d720d5b3e102bdd01805ce74fe
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 18:35:20 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './src/App';
+import MenuBar from './src/menu';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <MenuBar />
   </StrictMode>
 );
