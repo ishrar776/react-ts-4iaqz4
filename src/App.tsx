4d4e1835d6333a52a7a7891fecbeb3443611a273
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 18:33:32 +0530

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import '../style.css';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style.css';
+//import './style.css';
 //import MenuBar from './menu';
 export default function App() {
   let navigate = useNavigate();
