e714751cd1be517d38210c3c0fe40c0bc172d49c
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-31 18:35:51 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './src/App';
+//import App from './src/App';
 import MenuBar from './src/menu';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
